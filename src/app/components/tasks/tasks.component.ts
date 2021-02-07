75425f5604ac28cba5e05ea306fb3b4309d1da88
Anass el <anass.falah@hotmail.fr>
2021-02-07 02:42:09 +0100

    add crud functions

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import { Task } from 'src/app/models/task';
 
 @Component({
   selector: 'app-tasks',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
-  constructor() { }
+  myTask: Task = {
+    label: '',
+    status: false
+  }
+
+  tasks : Task[] = [];
+  constructor(private taskService : TaskService) { }
+
+  getData()
+  {
+    this.taskService.findAll().subscribe(tasks => this.tasks = tasks);
+  }
+
+  deleteTask(id){
+    this.taskService.delete(id)
+    .subscribe(() => this.tasks = this.tasks.filter(task => task.id != id));
+  }
+
+  persistTask(){
+    this.taskService.persist(this.myTask)
+    .subscribe((task) => {this.tasks = [task, ...this.tasks]});
+  }
 
   ngOnInit() {
+    this.getData();
+    //this.deleteTask(this.tasks);
   }
 
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor(private taskService: TaskService) { }
+  urlApi = "http://localhost:3000/tasks";
+  constructor(private httpClient: HttpClient) { }
+
+  findAll() {
+    return this.httpClient.get<Task[]>(this.urlApi);
+  }
+  
+  delete(id) {
+    return this.httpClient.delete(`${this.urlApi}/${id}`);
+  }
+
+  persist(task){
+    return this.httpClient.post<Task>(this.urlApi, task);
+  }
+
+  /* add(){
+    return this.httpClient.put(TasksComponent)
+  } */
+  
 }
